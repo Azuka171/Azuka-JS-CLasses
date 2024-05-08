37c2008ac9785b81f2a9dcd45c2d957f99a3df46
JideonwoFelix <99179966+JideonwoFelix@users.noreply.github.com>
2024-05-08 18:09:55 +0100

    #

--- Az-Classes/js-project/script.js
+++ Az-Classes/js-project/script.js
@@ -2,6 +2,32 @@
 
 document.getElementById('homelink').addEventListener('click', (event)=>{
         event.preventDefault()
-        console.log('link opening stopped', event.target)
+        switch_home()
     }
 )
+document.getElementById('aboutlink').addEventListener('click', (event)=>{
+        event.preventDefault()
+        switch_about()
+    }
+)
+document.getElementById('contactlink').addEventListener('click', (event)=>{
+        event.preventDefault()
+        switch_contact()
+    }
+)
+
+const switch_home = ()=>{
+    document.getElementsByTagName('main')[0].innerHTML = `   <main>
+        <h1>Home</h1>
+    </main>`
+}
+const switch_about = ()=>{
+    document.getElementsByTagName('main')[0].innerHTML = `   <main>
+        <h1>About</h1>
+    </main>`
+}
+const switch_contact = ()=>{
+    document.getElementsByTagName('main')[0].innerHTML = `   <main>
+        <h1>Contact</h1>
+    </main>`
+}
