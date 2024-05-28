6af56209c91773bee39a961c54ec9d32607096f9
Azuka171 <67867738+Azuka171@users.noreply.github.com>
2024-05-28 18:30:23 +0100

    cart page
    added some cart

--- E-commerce Javascript Project/JS/products.js
+++ E-commerce Javascript Project/JS/products.js
@@ -1,55 +1,73 @@
 const products = [
     {
         id: 1,
-        name: 'Event Planning ',
+        name: 'Event Planning:',
         img: './images/plan-image.jpg',
         dsc: 'We are skilled multitaskers, problem solvers, and creative thinkers we   love creating unforgettable experiences for our clients.We excel at organization, budget management, and communication, ensuring every detail is meticulously planned for a seamless event experience.',
         price : 350000
     },
     {
         id: 2,
-        name: 'Event Coverage',
+        name: 'Event Coverage:',
         img: './images/Live-Stream.jpg',
         dsc: ' From Pre-Event to Post Event, we are always able to create hype, capture the magic, relive the moments, and strengthen your brand.We also Go live on site  to allow you connect with your audience virtually wherever they may be!',
         price : 150000
     },
      {
         id: 3,
-        name: 'Decoration',
+        name: 'Decoration:',
         img: './images/dE1.jpeg',
-        dsc: 'Some Ladies like their sunglasses having big frames. It is a form of fashion and actually protects the eyes completely, not only from rays of the sun also from wind.This is one of those Designer Luxury Big Frame sunglasses with metal material and very comfortable to wear daily. It gives you a pretty look.',
+        dsc: 'We can transform your event from ordinary to extraordinary with various types of event decoration. From stunning table decorations to immersive digital backdrops, get inspired for your next event!',
         price : 50000
      },
     {
        id: 4,
-       name: 'Suprise Package',
+       name: 'Suprise Package:',
        img: './images/dE2s.jpeg',
-       dsc: '10x6 Inch Professional Graphics Drawing Tablet 12 Express Keys with 8192 Levels Battery-Free Stylus/8pcs Nibs/Pen Clip Support PC/Laptop Connection Compatible with Windows Mac for Painting Designing Online Course ',
-       price : 50000
+       dsc: 'We doubles the joy of any occasion with our  most unique and impressive surprise and gift ideas that can be creatively planned and organised for the most significant milestones of people’s lives. We have numerous surprise and gift packages  in store for all the occasions like birthdays, anniversaries, Christmas, engagements, baby showers, bridal showers, retirements and many more joyous occasions',
+       price : 10000
     },
     {
        id: 5,
-       name: 'Traditional Dressing',
+       name: 'Traditional Dressing:',
        img: './images/make-up.jpg',
-       dsc: '10x6 Inch Professional Graphics Drawing Tablet 12 Express Keys with 8192 Levels Battery-Free Stylus/8pcs Nibs/Pen Clip Support PC/Laptop Connection Compatible with Windows Mac for Painting Designing Online Course ',
+       dsc: 'We offer as many different costume traditions in Africa as there are languages. Each country, each tribe has clothing traditions that arise from local availability of materials, level of industrialization, trade partners, religions, weather, and cultural influences. There is no single traditional African costume. Africa has an exceedingly rich and varied costume landscape.',
        price : 50000
     },
     {  id: 6,
-       name: 'Cake',
+       name: 'Cake for all Occasions:',
        img: ' ./images/cakep1.jpeg',
-       dsc: '10x6 Inch Professional Graphics Drawing Tablet 12 Express Keys with 8192 Levels Battery-Free Stylus/8pcs Nibs/Pen Clip Support PC/Laptop Connection Compatible with Windows Mac for Painting Designing Online Course ',
+       dsc: 'We offer an Exceptional Cakes and other Bakery products that bring Joy and Sweet Memories to life’s Celebrations. ',
        price : 50000
     },
     {  id: 7,
-       name: 'Bridal MakeUp',
+       name: 'Bridal MakeUp:',
        img: ' ./images/new p29.jpg',
        dsc: 'A wedding this regal calls for a makeup look of equal refinement. Soft shadows and elegantly sculpted brows pair beautifully with hazelnut lipstick. Those long lashes, however, definitely steal the show.',
        price : 50000
     },
     {  id: 8,
-       name: 'Small Chops',
+       name: 'Small Chops:',
        img: './images/dEs1.jpeg',
-       dsc: '10x6 Inch Professional Graphics Drawing Tablet 12 Express Keys with 8192 Levels Battery-Free Stylus/8pcs Nibs/Pen Clip Support PC/Laptop Connection Compatible with Windows Mac for Painting Designing Online Course ',
+       dsc: 'We offer finger foods such as samosas, spring rolls, puff-puff, etc. They are usually used at occasions and events as appetizers, until the main meal comes. They are very delicious and can be eaten as snacks along with cold drinks, tea or water. ',
+       price : 50000
+    },
+    {  id: 9,
+       name: 'Wedding dress:',
+       img: './images/new p37.jpg',
+       dsc: 'We offer Unique and inexpensive wedding gowns that wow!',
+       price : 50000
+    },
+    {  id: 10,
+       name: 'Cocktails:',
+       img: './images/cocktails.jpg',
+       dsc: 'We offer classic, refreshing and sweet cocktails made with wine preferably red wine, fruit juice, and a variety of fruits.',
+       price : 50000
+    },
+    {  id: 11,
+       name: 'Food Tray:',
+       img: './images/pic20.jpg',
+       dsc: 'We deliver amazing food packages. this is best for occasions like birthdays, anniversaries etc.If you want to surprise your loved ones with something affordable and delicious , our premium food tray package will pay attention to your luxury and details and bring you mouth watering deliciousness.The package is affordable, and will bring you all the value and happiness you desire.',
        price : 50000
     },
 
