9eababf5a3abe38bcf702e7984faab73cebdaa18
JideonwoFelix <99179966+JideonwoFelix@users.noreply.github.com>
2024-05-30 04:51:14 +0100

    29May2024 class

--- E-commerce Javascript Project/JS/tewmp.js
+++ E-commerce Javascript Project/JS/tewmp.js
@@ -73,121 +73,24 @@ const products = [
 
 ]
 const Cart = []
-export function updateUI(){
-    const product_grid = document.getElementById('product-div')
-    if(product_grid===null){
-        return
+
+class CartItemClass{
+    constructor(id, name, img, price){
+        this.name = name;
+        this.name = name;
     }
-    products.map((product)=>{
-        const product_box = document.createElement('div');
-        product_box.classList = 'product-box'
-        const img = document.createElement('img');
-        img.src = product.img;
-        const product_name = document.createElement('div');
-        product_name.innerText = product.name;
-        product_name.classList='name'
-        const view_btn = document.createElement('button');
-        view_btn.innerText = 'More Details';
-        view_btn.addEventListener("click",  ()=>openModal(product))
-        const book_btn = document.createElement('button');
-        book_btn.innerText = 'Add to Cart'
-        book_btn.classList = 'btn'
-       
-
-        book_btn.addEventListener("click",  ()=>addToCart(product.id))
-        product_box.appendChild(img);
-        product_box.appendChild(product_name);
-        product_box.appendChild(view_btn);
-        product_box.appendChild(book_btn);
-
-        product_grid.appendChild(product_box);
-    })
-}
-updateUI() //initial function call
-function openModal(product_obj){
-    const modal = document.getElementsByClassName('modal_box')[0]
-    modal.getElementsByClassName('product_name')[0].innerHTML = product_obj.name
-    modal.getElementsByClassName('product_dsc')[0].innerHTML = product_obj.dsc
-    modal.querySelector('img').src = product_obj.img
-    modal.style.display = 'flex'
-
-}
-function closeModal(){
-    const modal = document.getElementsByClassName('modal_box')[0]
-    modal.style.display = 'none'
 }
 
-document.getElementsByClassName('overlay')[0].addEventListener('click', (event)=>{
-        event.preventDefault()
-        closeModal()
-    }
-)
-document.getElementsByClassName('btn-close')[0].addEventListener('click', (event)=>{
-    event.preventDefault()
-    closeModal()
-} )
 
 function addToCart(productId){
     // console.log(productId);
     const productIndex = products.findIndex(e =>e.id===productId)
     // console.log(products[productIndex]);
-    const tmp_p = products[productIndex]
-    const newObj = new CartItemClass(tmp_p.id,tmp_p.name, 'src/img.png', 3000)
     Cart.push(products[productIndex])
     console.table(Cart);
     updateCart();
 
 }
-function toggleDisplayCart(){
-    const cart_box = document.getElementById('cart-container');
-    if(cart_box.style.display==='none'){
-        cart_box.style.display = 'block'
-    }else{
-        cart_box.style.display = 'none'
-    }
-}
-document.getElementById('cartToggle').addEventListener('click', (event)=>{
-    toggleDisplayCart()
-})
 
-function updateCart(){
-    const cartDiv = document.getElementsByClassName('injectDiv')[0]
-    if(cartDiv===null){
-        return
-    }
-    cartDiv.innerHTML = '';
-    Cart.map((cartItem)=>{
-        const cart_containerBox = document.createElement('div');
-        cart_containerBox.classList = 'item-container'
-        const namebox = document.createElement('div');
-        namebox.innerText = cartItem.name;
-        cart_containerBox.appendChild(namebox);
-        const cartQty = document.createElement('div');
-        cartQty.classList = 'cart-qty-box'
-        const price = document.createElement('div');
-        price.innerText = cartItem.price;
-        cartQty.appendChild(price);
-        const btn_box = document.createElement('div');
-        btn_box.classList = 'cart-qty';
-        const button = document.createElement('button');
-        button.innerText ='-'
-        button.classList = 'btn-min'
-        btn_box.appendChild(button);
-        const number = document.createElement('div');
-        number.innerText = '2' //to revisit
-        btn_box.appendChild(number);
-        const lastbutton = document.createElement('button')
-        lastbutton.innerText = '+'
-        lastbutton.classList = 'btn-min'
-        btn_box.appendChild(lastbutton);
-
-        
-        cartQty.appendChild(btn_box);
-        cart_containerBox.appendChild(cartQty);
-        cartDiv.appendChild(cart_containerBox);
-
-
-    })
-}
 
 
